31ab6b1d65a27ac6319ac084dc01907cd8a9681f
Emmanuel Oluwasegun ADEYEYE <adeyeye1034@gmail.com>
2021-08-21 12:05:56 +0100

    adjustment

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,7 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/images/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     str(BASE_DIR.joinpath('staticfiles'))
 ]
