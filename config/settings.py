f806a1d2de73d4bc38544dec868f9b880204eb89
Emmanuel Oluwasegun ADEYEYE <adeyeye1034@gmail.com>
2021-08-21 12:29:36 +0100

    fixed

--- config/settings.py
+++ config/settings.py
@@ -123,10 +123,10 @@ if not DEBUG:
     STATIC_ROOT = ''
     
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static/'),
+        os.path.join(BASE_DIR, 'static'),
     ]
 # MEDIA_URL = '/images/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
