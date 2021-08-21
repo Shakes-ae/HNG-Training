6a033fd34eaed4162f9f97963df21e83863370fd
Emmanuel Oluwasegun ADEYEYE <adeyeye1034@gmail.com>
2021-08-21 12:28:08 +0100

    fixed

--- config/settings.py
+++ config/settings.py
@@ -119,13 +119,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+if not DEBUG:
+    STATIC_ROOT = ''
+    
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static/'),
+    ]
 # MEDIA_URL = '/images/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    str(BASE_DIR.joinpath('static'))
-]
+# STATICFILES_DIRS = [
+#     str(BASE_DIR.joinpath('static'))
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
